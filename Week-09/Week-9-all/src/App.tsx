a1284d4af4e8dd3b6e0feb5061ba80e8c84930e3
Sohaibkundi2 <sohaibkundi2@gmail.com>
2025-06-24 11:54:00 +0500

    practice of enum type interface generic etc

--- Week-09/Week-9-all/src/App.tsx
+++ Week-09/Week-9-all/src/App.tsx
@@ -14,6 +14,66 @@ function App() {
   console.log(fruits[1])
 
 
+// ************practice of generic enum import export***********
+
+
+  enum taskStatus{
+    Todo= "Todo",
+    InProgress= "InProgress",
+    Done= "Done"
+  }
+
+  interface Task{
+    title:string,
+    isCompleted:boolean,
+    status:taskStatus
+  }
+
+  const myTask: Task ={
+    title:"practice week 9",
+    isCompleted:false,
+    status:taskStatus.InProgress
+  }
+
+    // type is also same like interface but it is more extandable
+  
+  type user = {
+    name:string,
+    age:number
+  }
+  enum tech {
+    mobileUser= "mobileUser",
+    laptopUser= "laptopUser"
+  }
+  type techUser= user &{
+    userOf:tech
+  }
+
+  const Sohaib : techUser = {
+    name:"sohaib khan",
+    age: 19,
+    userOf:tech.laptopUser
+  }
+
+  // Generic
+
+  function wrappArray<T>(value:T):T[]{
+    return [value]
+  }
+
+  const a = wrappArray <string>("Sohaib")
+  const b = wrappArray <number>(20)
+
+  // export
+
+//   export default function add(a: number, b: number): number {
+//   return a + b;
+// }
+
+
+
+
+
   return (
     <>
       <div>
